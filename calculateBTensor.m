d752dd3a47a8d9e7bf582eb5ac299982ad23c9e3
Ricardo Rios <sleeping466@gmail.com>
2020-10-01 11:01:03 -0500

    Documenting, initialazing q variable

--- calculateBTensor.m
+++ calculateBTensor.m
@@ -1,11 +1,18 @@
 function [B, b_val] = calculateBTensor(g, dt)
+%calculateBTensor - Calculates B tensor from gradient waveform.
+% INPUTS:
 % g: gradient waveform, n*3, in T/m
 % d: step time for each point in gradient waveformt, in s
 
+% Author: Ricardo Rios
+% Note: Chunks of this code were taken from NOW toolbox
+% email:  ricardo.rios@cimat.mx
+
 gamma = 42.6e6*2*pi;
 
-% integrating from 0 to t each time
+% integrating waveform from 0 to t each time for q
 x = 0:dt:dt*size(g,1);
+q = zeros(size(g,1)-1,3);
 for i = 1:size(g,1)-1
     x_interval = x(1:i+1);
     g_interval = g(1:i+1,:);
