f89cfd408c57911d763ce5253d1f40d12195acbe
Ricardo Rios <sleeping466@gmail.com>
2020-10-01 12:19:47 -0500

    Main function, it executes the rest of the program in order

---
+++ nowToSequence.m
@@ -0,0 +1,45 @@
+function [Waveforms] = nowToSequence(problemNOW, resultNOW, param)
+% nowToSequence - Prints usable gradients files for MCW pulse sequences
+% toolbox from NOW objects from the NOW toolbox.
+% INPUTS:
+% problemNOW: problem optimization object. It comes from a NOW optimization
+% resultNOW: result structure. It comes from a NOW optimization
+% param: Structure with different input parameters for the pipeline:
+%   gMaxBruker - Max gradient strength on bruker sistem (T/m)
+%   rasterTime - Min raster time on bruker sistem- recommended 10us (s)
+%   desiredB - Desired b value for thw output waveform. It must not exceed
+%           gradient 100 strenght, a warining is displayed if that is the case
+%   tol - Tolerance for calculating the ddeired b value. 10 should do it
+%   directionVector - main direction used in the waveform. Needed if you
+%           plan to use the rotations on the MCW sequence toolbox. Check their wiki
+%   fileName - Filename of the output files
+
+% Author: Ricardo Rios
+% email:  ricardo.rios@cimat.mx
+
+% Scale waveform to desireed b value
+scaleFactor =  calculateScaleFactor(resultNOW.g*1e-3, resultNOW.dt, param.desiredB, param.tol);
+
+% Warnings if waveforms use too much gradients
+checkGradeintUse(resultNOW.g*1e-3,scaleFactor,param.gMaxBruker)
+
+% obtain waveform struct
+Waveforms=nowObjectsToWaveforms(problemNOW, resultNOW, param.rasterTime, scaleFactor);
+
+% save to file waveforms
+saveWaveformFile(Waveforms, param.gMaxBruker, param.directionVector, param.fileName)
+
+end
+
+function checkGradeintUse(g, scaleFactor, gMaxBruker)
+% checkGradeintUse - checks percentage gradient strength used, trows a
+% warning if 100% is exceded
+g = g*scaleFactor./gMaxBruker;
+gVec = g(:);
+
+maxGradientUsed = max(abs(gVec))*100;
+fprintf('Max gradient power used: %f %% \n',maxGradientUsed)
+if maxGradientUsed > 100
+    warning("Excedding 100% gradient strength for the desired B_value")                
+end
+end
